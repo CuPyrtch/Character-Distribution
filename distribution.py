1dff8c4af71a2a464171077576331eb9d28e6269
CuPyrtch <46973325+CuPyrtch@users.noreply.github.com>
2019-04-01 21:44:04 -0400

    Updated from Brython Server: 4/1/2019 9:44:04 PM

--- distribution.py
+++ distribution.py
@@ -60,3 +60,28 @@ for i in range(0, len(answer)):
 
 
 
+def compare(a, b):
+    """
+    compare - generic comparison function for testing two elements.
+    """
+    return a > b
+
+
+def bsort(seq, cmp):
+    """
+    bsort - simple sorting algorithm that uses any comparison function
+    seq - a list to be sorted
+    cmp - a function for comparing two elements of seq
+    """
+    sorted = False  # assume the seq is not sorted to start with
+    while not sorted:
+        sorted = True   # assume it's already sorted correctly
+        for index, value in enumerate(seq): # for every element in seq
+            if index > 0:                   # past the first..
+                if not cmp(seq[index-1], value):  # if this element is out of order
+                    sorted = False          # then the list is not sorted yet
+                    seq[index-1], seq[index] = seq[index], seq[index-1] # and swap it
+
+    
+bsort(answer, compare)
+print(answer)
